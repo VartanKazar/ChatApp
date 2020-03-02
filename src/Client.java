c2ba96058d3c67d391c6fb2005e84e70910689ce
kiyanh <kiyan.haddad.267@my.csun.edu>
2020-03-02 12:24:39 -0800

    finished client class

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,9 @@
-import java.net.*;
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 //The main part of the project.  Allows those who run this class to connected to an already opened TCP channel, given an ip, port, and a user name.
 public class Client {
@@ -14,10 +17,10 @@ public class Client {
     private String server, userName;
     private int port;
 
-    /*Custom constructor which takes the following parameters to allow the user to connect to a custom server with a designated port.
-        server:  The address of the server for the user to connect to.
-        port:  The port number of the server
-     */
+    /* Custom constructor which takes the following parameters to allow the user to connect
+       to a custom server with a designated port. The address of the server for the user to connect to.
+       The port holds the port number of the server
+    */
     Client(String server, int port, String userName){
         this.server = server;
         this.port = port;
@@ -25,7 +28,9 @@ public class Client {
     }
 
     /*Default constructor
-    server:  By default, if no ip address is supplied to the client on construction, then it is assumed the client will be hosting the server.  So server is set to hosts local ip.  Can always change these details later.
+    server:  By default, if no ip address is supplied to the client on construction,
+    then it is assumed the client will be hosting the server.  So server is set to hosts local ip.
+    Can always change these details later.
      */
     Client() throws UnknownHostException {
 
@@ -39,6 +44,66 @@ public class Client {
 
     }
 
+    public boolean start() {
+// try to connect to the server
+        try {
+            socket = new Socket(server, port);
+        }
+        catch(Exception ec) {
+            System.out.print("Error connecting to server:" + ec);
+            return false;
+        }
+        String msg = "Connection accepted " + socket.getInetAddress() + ":" + socket.getPort();
+        System.out.print(msg);
+        /* Creating both Data Stream */
+        try
+        {
+            input  = new ObjectInputStream(socket.getInputStream());
+            output = new ObjectOutputStream(socket.getOutputStream());
+        }
+        catch (IOException eIO) {
+            System.out.print("Exception creating new Input/output Streams: " + eIO);
+            return false;
+        }
+
+        // creates the Thread to listen from the server
+        //new ListenFromServer().start();
+        try {
+            msg = (String) input.readObject();
+            System.out.println(msg);
+            System.out.print("> ");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        // Send our username to the server this is the only message that we
+        // will send as a String. All other messages will be ChatMessage objects
+        try
+        {
+            output.writeObject(userName);
+        }
+        catch (IOException eIO) {
+            System.out.print("Exception doing login : " + eIO);
+            disconnect();
+            return false;
+        }
+        // success we inform the caller that it worked
+        return true;
+    }
+
+    void sendMessage(ChatMessage msg) {
+        try {
+            output.writeObject(msg);
+        }
+        catch(IOException e) {
+            System.out.print("Exception writing to server: " + e);
+        }
+    }
+
     public String getServerAddress() { return server; }
     public void setServerAddress(String address) { server = address; }
 
@@ -46,4 +111,21 @@ public class Client {
     public void setServerPort(int port) { this.port = port; }
 
     public String getMyIp() throws UnknownHostException {return InetAddress.getLocalHost().getHostAddress();}
+
+    private void disconnect() {
+        try {
+            if(input != null) input.close();
+        }
+        catch(Exception e) {}
+        try {
+            if(output != null) output.close();
+        }
+        catch(Exception e) {}
+        try{
+            if(socket != null) socket.close();
+        }
+        catch(Exception e) {}
+
+    }
+
 }
